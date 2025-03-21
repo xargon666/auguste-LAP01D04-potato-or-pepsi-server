f4d3fa2c3d4a1babda1520267c0e24af2cff720f
Chris Pucknell <chris.pucknell@gmail.com>
2025-03-21 15:56:22 +0000

    update port console log

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const server = require('./server')
 
 const port = process.env.PORT || 3000
 
-server.listen(port, () => console.log(`\nExpress departing now from port 3000!\n`))
+server.listen(port, () => console.log(`\nExpress departing now from port ${port}!\n`))
